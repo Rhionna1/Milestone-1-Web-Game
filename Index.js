dba2b53a67a29a2ee3802ff08abaa0fcdc988060
Rhionna Landers <kasmiere@Rhionnas-Mac-mini.local>
2023-08-07 21:01:53 -0500

    add paddle 1 postion

--- Index.js
+++ Index.js
@@ -16,4 +16,19 @@ const ballRadius = 12.5;
 const paddleSpeed = 60;
 //constant variables end here
 //declaring other variables start here
-
+let intervalID; 
+let ballSpeed = 1;
+let ballX = gameWidth / 2;
+let ballY = gameHeight / 2;
+let ballXDirection = 0;
+let ballYDirection = 0;
+let player1Score = 0;
+let player2Score =0;
+//declaring other variables end here
+//paddle 1 position starts here
+let paddle1 = {
+    width:
+}
+//paddle 1 position ends here
+//paddle 2 position starts here
+//paddle 2 position ends here
