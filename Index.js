86ce0fd900b4c31fb9f28976c715abf0c804d6ae
Rhionna Landers <kasmiere@Rhionnas-Mac-mini.local>
2023-08-07 19:32:53 -0500

    added first paddle to JS

--- Index.js
+++ Index.js
@@ -1,4 +1,16 @@
-//paddle game background//
+//paddle game background
+
 paddleGame ({
     background: [35, 122, 27],
 })
+
+//add paddles to game
+add ([
+    pos (40,0),
+    rect (20, 80),
+    outline (4),
+    anchor ("center"),
+    area (),
+    "paddle",
+])
+
