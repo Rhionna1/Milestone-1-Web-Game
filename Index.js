b3fe01ffd1b35f335d65c3af43de0b855fbb12ef
Rhionna Landers <kasmiere@Rhionnas-Mac-mini.local>
2023-08-07 20:53:56 -0500

    adding more constant variable

---
+++ Index.js
@@ -0,0 +1,19 @@
+//declaring constant variables
+
+const gameBoard = document.querySelector (#gameBoard);
+const ctx = gameBoard.getContext ("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "forestgreen";
+const paddle1Color = "lightblue"; 
+const paddle2Color = "red"; 
+const paddleBoarder = "black";
+const ballColor = "yellow";
+const ballBoarderColor: "black";
+const ballRadius = 12.5;
+const paddleSpeed = 60;
+//constant variables end here
+//declaring other variables start here
+
