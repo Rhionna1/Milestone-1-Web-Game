5d3d14e3165ef250a1c55aad9c4245e638945790
Rhionna Landers <kasmiere@Rhionnas-Mac-mini.local>
2023-08-14 23:01:19 -0500

    added the details via Javascript to the game board

--- Index.js
+++ Index.js
@@ -3,19 +3,19 @@ var DIRECTION = {
     IDLE: 0,
     UP: 1,
     DOWN: 2, 
-    LEFT 3, 
-    RIGHT 4
+    LEFT: 3, 
+    RIGHT: 4
 };
 
 var rounds = [5,5,3,3,2];
-var colors = ['#12104B, #237A1B, #AA66DD, #8c52ff, #2ecc71'];
+var colors = ['#12104B', '#237A1B', '#AA66DD', '#8c52ff', '#2ecc71'];
 
 //function for ball
 var Ball = {
     new: function (incrementedSpeed) {
         return {
-            width: 18
-            height: 18
+            width: 18,
+            height: 18,
             x: (this.canvas.width /2)-9,
             y: (this.canvas.height /2)-9,
             moveX: DIRECTION.IDLE, 
@@ -40,11 +40,34 @@ var Ai = {
         };
     }
 };
-//calling 
+// function that creates the gameboard
 var Game = {
     initialize: function () {
         this.canvas = document.querySelector('canvas');
         this.context = this.canvas.getContext('2d');
+
+        this.canvas.width = 1400;
+        this.canvas.height =1000;
+
+        this.canvas.style.width = (this.canvas.width / 2) + 'px';
+        this.canvas.style.height = (this.canvas.height / 2) + 'px';
+
+        this.player = Ai.new.call(this, 'left');
+        this.ai = Ai.new.call(this, 'right');
+        this.ball = Ball.new.call(this); 
+
+        this.ai.speed = 5;
+        this.running = this.over = false; 
+        this.turn = this.ai;
+        this.timer = this.round = 0; 
+        this.color = '#8c52ff';
+
+        Pong.menu();
+        Pong.listen();
+    }
+//at the end of the game the canva, font size, and color will change
+    endGameMenu: function(text) {
+
     }
 };
 
